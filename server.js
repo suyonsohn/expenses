ca91a9c906f45072111999338372e3a95848b139
sys <suyonsohn@gmail.com>
2019-04-16 12:34:12 -0700

    Add jwt check to private route

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ const express = require("express");
 
 const app = express();
 
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev--yscf3ip.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'expenses-api',
+  issuer: 'https://dev--yscf3ip.auth0.com/',
+  algorithms: ['RS256']
+});
+
 app.get("/public", (req, res) => {
   res.status(200).send("Hello PUBLIC expenses!");
 });
 
-app.get("/private", (req, res) => {
+app.get("/private", jwtCheck, (req, res) => {
   res.status(200).send("Hello PRIVATE expenses!");
 });
 
