0e744b8f4686c49ffae33bafe7bc3f9e2a058619
sys <suyonsohn@gmail.com>
2019-05-10 17:28:10 -0700

    Add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const privateKey = require("../config/keys");
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "Missing token, authorization denied" });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, privateKey);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token, authorization denied" });
+  }
+};
