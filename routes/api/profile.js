117494636768d90437437d1bbb05e49871df1444
sys <suyonsohn@gmail.com>
2019-05-12 10:19:26 -0700

    GET api/profile/user/:user_id - Get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,4 +102,25 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Private
+router.get("/user/:user_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
