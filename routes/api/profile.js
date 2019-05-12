262606a6e70c2e4967275215e7ee294627704bb7
sys <suyonsohn@gmail.com>
2019-05-12 12:26:08 -0700

    DELETE api/profile - Delete profile, user, posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -123,4 +123,20 @@ router.get("/user/:user_id", auth, async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user, posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // TODO: Delete user's posts
+
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "Profile deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
