f583cb0e4bb59adea0efe5ce63e880a6cc3a9fbb
sys <suyonsohn@gmail.com>
2019-05-14 22:16:35 -0700

    Set up redux store

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,26 @@ import Landing from "./components/layout/Landing";
 import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
